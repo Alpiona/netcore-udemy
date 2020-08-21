3bee6c88234e3d027eb0af785cbcdc1905a5091d
Alpiona <alpiona@hotmail.com>
2020-08-21 16:58:53 -0300

    Activity Controller ready and working

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
@@ -23,16 +21,16 @@ namespace DatingApp.API.Controllers
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Value>>> Get()
     {
-        var values = await _context.Values.ToListAsync();
-        return Ok(values);
+      var values = await _context.Values.ToListAsync();
+      return Ok(values);
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
     public async Task<ActionResult<string>> Get(int id)
     {
-        var value = await _context.Values.FindAsync(id);
-        return Ok(value);
+      var value = await _context.Values.FindAsync(id);
+      return Ok(value);
     }
 
     // POST api/values
